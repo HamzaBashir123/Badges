5f65085ef5bfdf22a5af633fed5ab64782878525
hamzabashir1268@gmail.com <hamzabashir1268@gmail.com>
2024-05-27 06:36:06 +0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Checck the combiantion of 2 key
+// if (key1 == key2) {
+    // If the key is the same, then the key is the same
+    // \end{code}
+    // \end{code}
+    // \end{code}
+    // \end{code}
+    // \end{code}
