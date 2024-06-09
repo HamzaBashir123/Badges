9d138635b68f33263790bd5f1593717264ac50cc
hamzabashir1268@gmail.com <hamzabashir1268@gmail.com>
2024-06-09 08:14:47 +0500

    updae

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ firstH1.innerHTML = "second new";
 
 // Checck the combiantion of 2 key
 // if (key1 == key2) {
-// If the key is the same, then the key is the same
-// \end{code}
-// \end{code}
-// \end{code}
-// \end{code}
-// \end{code}
+
 
 // \end{code}
 
