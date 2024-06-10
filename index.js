283ef9ef5496742ad425a758f245d2a34e06c4a8
hamzabashir1268@gmail.com <hamzabashir1268@gmail.com>
2024-06-10 07:28:25 +0500

    update

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ firstH1.innerHTML = "second new";
 // let b = prompt('Please enter your name')
 // let b = prompt('Please enter your name')
 // let b = prompt('Please enter your name')
+// let b = prompt('Please enter your name')
 
 
 
